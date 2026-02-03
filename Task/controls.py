e88665f7e2344d177d61a0d3c2b51e932e8b07e6
oRioN-Genics <sathsara2021@gmail.com>
2026-02-03 18:03:45 +0530

    feat: implemented camera stream, mavlink connection, simple state machine,  and basic takeoff and landing for testing under 'GUIDED','LAND' modes

---
+++ Task/controls.py
@@ -0,0 +1,43 @@
+import time
+from pymavlink import mavutil
+
+def force_arm(master): 
+    master.mav.command_long_send(
+        master.target_system,
+        master.target_component,
+        mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
+        0,
+        1,      # 1 to arm, 0 to disarm
+        21196,  # Force-arm magic number for ArduPilot
+        0, 0, 0, 0, 0
+    )
+    return True
+
+def takeoff(master, target_alt):
+    master.mav.command_long_send(
+        master.target_system,
+        master.target_component,
+        mavutil.mavlink.MAV_CMD_NAV_TAKEOFF,
+        0,
+        0, 0, 0, 0,
+        0, 0,
+        target_alt
+    )
+
+class AltitudeTracker:
+    def __init__(self):
+        self.stable_count = 0
+        self.last_alt = None
+
+    def update(self, master, target_alt, tol):
+        msg = master.recv_match(type='GLOBAL_POSITION_INT', blocking=False)
+        if not msg:
+            return False, self.last_alt
+        
+        self.last_alt = msg.relative_alt / 1000.0
+        if self.last_alt >= (target_alt - tol):
+            self.stable_count += 1
+        else:
+            self.stable_count = 0
+
+        return self.stable_count >= 10, self.last_alt
